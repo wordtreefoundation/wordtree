1d212467c280849229a76b73b5152511b14d2a9a
thatchrischris <thatchrischris@gmail.com>
2014-01-26 16:01:46 -0700

    Updated Wording, Pop-ups and Color Theme

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -733,20 +733,6 @@ var Index = function () {
             }, 2000);
 
             setTimeout(function () {
-                var unique_id = $.gritter.add({
-                    // (string | mandatory) the heading of the notification
-                    title: 'Now Powered by MeteorJS',
-                    // (string | mandatory) the text inside the notification
-                    text: 'The WordTree Foundation has decided to dump Ruby in favor of NodeJS based technology, what will they find as they venture into lands uncharted?',
-                    // (string | optional) the image to display on the left
-                    image: './assets/img/avatar1.jpg',
-                    // (bool | optional) if you want it to fade out on its own or just sit there
-                    sticky: true,
-                    // (int | optional) the time you want it to be alive for before fading out
-                    time: '',
-                    // (string | optional) the class name you want to apply to that specific message
-                    class_name: 'my-sticky-class'
-                });
 
                 // You can have it return a unique id, this can be used to manually remove it later using
                 setTimeout(function () {
@@ -782,92 +768,8 @@ var Index = function () {
 
             }, 23000);
 
-            setTimeout(function () {
-
-                $.extend($.gritter.options, {
-                    position: 'top-left'
-                });
-
-                var unique_id = $.gritter.add({
-                    // (string | mandatory) the heading of the notification
-                    title: 'Notification',
-                    // (string | mandatory) the text inside the notification
-                    text: 'You have 3 new notifications.',
-                    // (string | optional) the image to display on the left
-                    image1: './assets/img/image1.jpg',
-                    // (bool | optional) if you want it to fade out on its own or just sit there
-                    sticky: true,
-                    // (int | optional) the time you want it to be alive for before fading out
-                    time: '',
-                    // (string | optional) the class name you want to apply to that specific message
-                    class_name: 'my-sticky-class'
-                });
-
-                setTimeout(function () {
-                    $.gritter.remove(unique_id, {
-                        fade: true,
-                        speed: 'slow'
-                    });
-                }, 4000);
-
-                $.extend($.gritter.options, {
-                    position: 'top-right'
-                });
-
-                var number = $('#header_notification_bar .badge').text();
-                number = parseInt(number);
-                number = number + 3;
-                $('#header_notification_bar .badge').text(number);
-                $('#header_notification_bar').pulsate({
-                    color: "#66bce6",
-                    repeat: 5
-                });
-
-            }, 40000);
-
-            setTimeout(function () {
-
-                $.extend($.gritter.options, {
-                    position: 'top-left'
-                });
-
-                var unique_id = $.gritter.add({
-                    // (string | mandatory) the heading of the notification
-                    title: 'Inbox',
-                    // (string | mandatory) the text inside the notification
-                    text: 'You have 2 new messages in your inbox.',
-                    // (string | optional) the image to display on the left
-                    image1: './assets/img/avatar1.jpg',
-                    // (bool | optional) if you want it to fade out on its own or just sit there
-                    sticky: true,
-                    // (int | optional) the time you want it to be alive for before fading out
-                    time: '',
-                    // (string | optional) the class name you want to apply to that specific message
-                    class_name: 'my-sticky-class'
-                });
-
-                $.extend($.gritter.options, {
-                    position: 'top-right'
-                });
-
-                setTimeout(function () {
-                    $.gritter.remove(unique_id, {
-                        fade: true,
-                        speed: 'slow'
-                    });
-                }, 4000);
-
-                var number = $('#header_inbox_bar .badge').text();
-                number = parseInt(number);
-                number = number + 2;
-                $('#header_inbox_bar .badge').text(number);
-                $('#header_inbox_bar').pulsate({
-                    color: "#dd5131",
-                    repeat: 5
-                });
-
-            }, 60000);
-        }
+         
+		}
 
     };
 
